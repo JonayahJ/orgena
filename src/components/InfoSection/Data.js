0efe62f9b4fb2882eb4b515f1569af84ddc4df3f
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-06-05 00:59:36 -0400

    about section on homepage layout done

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+
+export const homeObjOne = {
+    id: "about",
+    lightBG: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: "About",
+    headline: "Who are we?",
+    description: "An experienced advisor that assists in helping you make efficient decisions as you grow and develop your business.",
+    buttonLabel: "Learn more",
+    imgStart: false,
+    img: ("../../images/svg-1.svg"),
+    alt: "Shake Hands",
+    dark: true,
+    primary: true,
+    darkText: false,
+}
---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Button } from '../ButtonElements'
+import { 
+    InfoContainer, 
+    InfoWrapper,
+    InfoRow, 
+    Column1, 
+    Column2, 
+    TextWrapper, 
+    TopLine, 
+    Heading, 
+    Subtitle, 
+    BtnWrap, 
+    Img, 
+    ImgWrap 
+} from './InfoElements'
+
+const InfoSection = ({ lightBG, id, imgStart, topLine, lightText, headline, darkText, description, buttonLabel, img, alt }) => {
+    return (
+        <>
+            <InfoContainer lightBG={lightBG} id={id}>
+                <InfoWrapper>
+                    <InfoRow imgStart={imgStart}>
+                        <Column1>
+                            <TextWrapper>
+                                <TopLine>{topLine}</TopLine>
+                                <Heading lightText={lightText}>{headline}</Heading>
+                                <Subtitle darkText={darkText}>{description}</Subtitle>
+                                <BtnWrap>
+                                    <Button to="/about">{buttonLabel}</Button>
+                                </BtnWrap>
+                            </TextWrapper>
+                        </Column1>
+                        <Column2>
+                            <ImgWrap>
+                                <Img src={img} alt={alt} />
+                            </ImgWrap>
+                        </Column2>
+                    </InfoRow>
+                </InfoWrapper>
+            </InfoContainer>   
+        </>
+    )
+}
+
+export default InfoSection
