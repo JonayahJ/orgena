457292ac81cc068f961e5880d9c6bc7be9ec76f8
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-06-05 10:48:40 -0400

    set the other info sections

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -54,7 +54,7 @@ export const TextWrapper = styled.div`
 `
 
 export const TopLine = styled.p`
-    color: var(--bitter-lemon);
+    color: ${({topText}) => (topText ? "var(--bitter-lemon)" : "var(--satin-sheen-gold)")};
     font-size: 16px;
     line-height: 16px;
     font-weight: 700;
--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -15,7 +15,7 @@ import {
     ImgWrap 
 } from './InfoElements'
 
-const InfoSection = ({ lightBG, id, imgStart, topLine, lightText, headline, darkText, description, buttonLabel, img, alt }) => {
+const InfoSection = ({ lightBG, id, imgStart, topLine, lightText, headline, darkText, description, buttonLabel, img, alt, primary, dark, dark2, topText }) => {
     return (
         <>
             <InfoContainer lightBG={lightBG} id={id}>
@@ -23,11 +23,16 @@ const InfoSection = ({ lightBG, id, imgStart, topLine, lightText, headline, dark
                     <InfoRow imgStart={imgStart}>
                         <Column1>
                             <TextWrapper>
-                                <TopLine>{topLine}</TopLine>
+                                <TopLine topText={topText} >{topLine}</TopLine>
                                 <Heading lightText={lightText}>{headline}</Heading>
                                 <Subtitle darkText={darkText}>{description}</Subtitle>
                                 <BtnWrap>
-                                    <Button to="/about">{buttonLabel}</Button>
+                                    <Button 
+                                        to="/about"
+                                        primary={ primary ? 1 : 0 }
+                                        dark={ dark ? 1 : 0 }
+                                        dark2={ dark2 ? 1 : 0 }
+                                    >{buttonLabel}</Button>
                                 </BtnWrap>
                             </TextWrapper>
                         </Column1>
