3ee187694adb3343a67e751b1b62af1e2ff087ad
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-06-20 11:48:33 -0400

    finished footer setup

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,20 @@ import {
     FooterLinksWrapper, 
     FooterLinkItems, 
     FooterLinkTitle, 
-    FooterLink 
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
 } from "./FooterElements"
+import {
+    FaFacebook,
+    FaGooglePlusG,
+    FaInstagram,
+    FaTwitter,
+  } from 'react-icons/fa';
 
 const Footer = () => {
     return (
@@ -18,18 +30,18 @@ const Footer = () => {
                         {/* About */}
                         <FooterLinkItems>
                             <FooterLinkTitle>About</FooterLinkTitle>
-                                <FooterLink to="#">About Us</FooterLink>
-                                <FooterLink to="#">Consultants</FooterLink>
-                                <FooterLink to="#">Services</FooterLink>
-                                <FooterLink to="#">Testimonials</FooterLink>
+                                <FooterLink to="/about-us">About Us</FooterLink>
+                                <FooterLink to="/consultants">Consultants</FooterLink>
+                                <FooterLink to="/services">Services</FooterLink>
+                                <FooterLink to="/testimonials">Testimonials</FooterLink>
                         </FooterLinkItems>
                         {/* Contact */}
                         <FooterLinkItems>
                             <FooterLinkTitle>Contact</FooterLinkTitle>
-                                <FooterLink to="#">Conact Us</FooterLink>
-                                <FooterLink to="#">Talk With Us</FooterLink>
-                                <FooterLink to="#">Phone</FooterLink>
-                                <FooterLink to="#">Email</FooterLink>
+                                <FooterLink to="/schedule">Book Consultation</FooterLink>
+                                <FooterLink to="/contact">Contact Us</FooterLink>
+                                <a href="mailto:support@orgenaconsulting.com" className="footerLink">Email Us</a>
+                                <a href="tel:1234567890" className="footerLink">+1 (123)456-7890</a>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
@@ -44,13 +56,36 @@ const Footer = () => {
                         {/* Legal */}
                         <FooterLinkItems>
                             <FooterLinkTitle>Legal</FooterLinkTitle>
-                                <FooterLink to="#">Cookie Policy</FooterLink>
-                                <FooterLink to="#">Terms of Service</FooterLink>
-                                <FooterLink to="#">Privacy Policy</FooterLink>
+                                {/* <FooterLink to="#">Cookie Policy</FooterLink> */}
+                                <FooterLink to="/terms">Terms of Service</FooterLink>
+                                <FooterLink to="/privacy">Privacy Policy</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
-                
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/">
+                            <img src="logo_white.png" height="100px" width="auto" alt="Orgena Consulting logo" />
+                        </SocialLogo>
+                        <WebsiteRights>
+                            Orgena Consulting © 2020 - {new Date().getFullYear()} All rights reserved. 
+                        </WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank" rel="noopenner noreferrer" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" rel="noopenner noreferrer" aria-label="Google+">
+                                <FaGooglePlusG />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" rel="noopenner noreferrer" aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" rel="noopenner noreferrer" aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
---
+++ src/components/Privacy/Data.js
@@ -0,0 +1,6 @@
+// Content for Privacy Policy from Get Terms
+export const PrivacyText = {
+    id: "privacy-policy",
+}
+
+// RAW
