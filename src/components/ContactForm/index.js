03cb4d9ceaa5f042330be272d532ef78ec4d4d2b
Jonayah Jackson <jonayah@thinkhalcyon.com>
2021-06-20 13:46:37 -0400

    setting htmlFor

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -19,11 +19,11 @@ const Contact = () => {
               <Form method="post" name="Contact Form" netlify data-netlify="true">
                 <FormH1>Send us a message</FormH1>
                 <Text>Have a question? Need to reach out?  Use the form below to get in touch.</Text>
-                <FormLabel htmlFor='for'>Your Name</FormLabel>
+                <FormLabel htmlFor='name'>Your Name</FormLabel>
                 <FormInput type='name' name="name" required />
-                <FormLabel htmlFor='for'>Email</FormLabel>
+                <FormLabel htmlFor='email'>Email</FormLabel>
                 <FormInput type='email' name="email" required />
-                <FormLabel htmlFor='for'>Message</FormLabel>
+                <FormLabel htmlFor='message'>Message</FormLabel>
                 <FormTextArea type='textarea' name="message" required />
                 <FormButton type='submit'>Submit</FormButton>
               </Form>
